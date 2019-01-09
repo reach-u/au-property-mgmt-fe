c9d5be8558ce16390170ca720090513f72064ecf
Kristian Hunt <kristian.hunt@reach-u.com>
2019-01-09 11:12:03 +0200

    RUSS-51 Add expanding rows

--- src/views/admin/MonthStats.jsx
+++ src/views/admin/MonthStats.jsx
@@ -1,11 +1,14 @@
 import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
 
 import '../navigation.scss';
 import '../registry/registry.scss';
+import '../registry/results.css';
 import './monthlystats.scss';
+import school from '../../assets/baseline-school-24px.svg';
 import { observer } from 'mobx-react';
 import api from '../../config/API';
-import { Classes } from "@blueprintjs/core";
+import { Icon, Classes } from "@blueprintjs/core";
 import { BarChart, d3 } from 'react-d3-components';
 
 class MonthStats extends Component {
@@ -17,6 +20,15 @@ class MonthStats extends Component {
         };
     }
 
+    handleResize(e) {
+        let elem = ReactDOM.findDOMNode(this);
+        let width = elem.offsetWidth;
+
+        this.setState({
+            parentWidth: width
+        });
+    }
+
     componentDidMount() {
         fetch(`${window.location.origin}/${api.getLandTaxMonthlyStats()}`)
             .then(response => {
@@ -26,15 +38,24 @@ class MonthStats extends Component {
                 return response.json()
             })
             .then(data => {
+                data.forEach(function (monthlyTaxData) {
+                    monthlyTaxData.expanded = false;
+                    monthlyTaxData.indebtedOwners.forEach(function (debtorMonthlyData) {
+                        debtorMonthlyData.isAddressesExpanded = false;
+                    });
+                });
                 this.setState({ taxInfoList: data })
             })
             .catch(error => {
                 console.error(error);
             });
+        this.handleResize();
     }
 
     render() {
         let total = this.calculateTotal();
+        const { authstore } = this.props;
+
         return (
             <Fragment>
                 <div className="monthly-tax-container">
@@ -43,8 +64,11 @@ class MonthStats extends Component {
                             className={[Classes.HTML_TABLE_STRIPED, Classes.HTML_TABLE].join(' ')}>
                             <thead>
                                 <tr>
-                                    <th>
-                                        Month name
+                                    <th className="text-row">
+                                        Debtors
+                                    </th>
+                                    <th className="text-row">
+                                        Month
                                     </th>
                                     <th>
                                         Amount (plan)
@@ -57,25 +81,18 @@ class MonthStats extends Component {
                                     </th>
                                 </tr>
                             </thead>
-                            <tbody>
-                                {this.state.taxInfoList.map((taxInfo, index) => (
-                                    <tr key={index}>
-                                        <td title="Month name">
-                                            {taxInfo.name}
-                                        </td>
-                                        <td title="planned amount">
-                                            {taxInfo.plannedAmount.toLocaleString('en-US')}
-                                        </td>
-                                        <td title="paid amount">
-                                            {taxInfo.paidAmount.toLocaleString('en-US')}
-                                        </td>
-                                        <td title="missing amount">
-                                            {taxInfo.missingAmount.toLocaleString('en-US')}
-                                        </td>
-                                    </tr>
-                                ))}
+                            {this.state.taxInfoList.map((taxInfo, index) => (
+                                [<tbody key={index}>{this.getTableDataRow(taxInfo)}</tbody>,
+                                this.getTableExpandedRows(taxInfo, authstore)
+                                ]
+
+                            ))}
+                            <tbody key="total-row">
                                 <tr className="total-row">
-                                    <td title="Total">
+                                    <td>
+
+                                    </td>
+                                    <td title="Total" className="text-row">
                                         Total
                                     </td>
                                     <td title="planned amount">
@@ -98,6 +115,143 @@ class MonthStats extends Component {
             </Fragment>
         )
     }
+
+    getTableDataRow = (taxInfo) => {
+        return (
+            <tr key={taxInfo.name}>
+                <td className="text-row">
+                    {taxInfo.missingAmount > 0 &&
+                        <span className="open-debtors" onClick={() => this.handleExpandDebtorsTable(taxInfo)} title="Expand">
+                            <Icon icon={taxInfo.expanded ? "chevron-down" : "chevron-right"} />
+                        </span>
+                    }
+                </td>
+                <td title="Month name" className="text-row">
+                    {taxInfo.name}
+                </td>
+                <td title="planned amount">
+                    {taxInfo.plannedAmount.toLocaleString('en-US')}
+                </td>
+                <td title="paid amount">
+                    {taxInfo.paidAmount.toLocaleString('en-US')}
+                </td>
+                <td title="missing amount">
+                    {taxInfo.missingAmount.toLocaleString('en-US')}
+                </td>
+            </tr>);
+    };
+
+    getTableExpandedRows = (taxInfo, authstore) => {
+        var indebtedOwners = taxInfo.indebtedOwners;
+        var debtorAddresses = taxInfo.debtorAddresses;
+
+        var rowArray = [
+            <tr key={taxInfo.name + '_expanded'} className={taxInfo.expanded ? "show-row" : "hide-row"}>
+                <th className="text-row">
+                    Properties
+                </th>
+                <th className="text-row">
+                    Owner name
+                </th>
+                <th>
+                    Amount paid
+                </th>
+                <th>
+                    Amount missing
+                </th>
+                <th>
+                    Send reminder
+                </th>
+            </tr>
+        ];
+
+        indebtedOwners.map(owner =>
+            [rowArray.push(
+                <tr key={owner.name + '_owner'} className={taxInfo.expanded ? "show-row" : "hide-row"}>
+                    <td className="text-row">
+                        <span className="open-addresses" onClick={() => this.handleExpandAddressesTable(taxInfo, owner)} title="Expand">
+                            <Icon icon={owner.isAddressesExpanded ? "chevron-down" : "chevron-right"} />
+                        </span>
+                    </td>
+                    <td className="text-row">
+                        {authstore.getUsernameById(owner.name)}
+                    </td>
+                    <td>
+                        {owner.paidAmount.toLocaleString('en-US')}
+                    </td>
+                    <td>
+                        {owner.missingAmount.toLocaleString('en-US')}
+                    </td>
+                    <td>
+                        <button className="reminder-button" onClick={() => this.handleReminderButtonClick()}>Send reminder</button>
+                    </td>
+                </tr>
+            ),
+            rowArray.push(
+                <tr key={owner.name + '_properties'} className={owner.isAddressesExpanded ? "show-row" : "hide-row"}>
+                    <th></th>
+                    <th className="text-row">
+                        Property
+                    </th>
+                    <th className="text-row">
+                        Address
+                    </th>
+                    <th>Amount missing</th>
+                    <th></th>
+                </tr>
+            ),
+            rowArray.push(debtorAddresses[owner.name].map(address =>
+                <tr key={address.id} className={owner.isAddressesExpanded ? "show-row" : "hide-row"}>
+                    <td>
+                        {address.detailedData.buildingType === 'School' ? (
+                            <img src={school} alt="School" />
+                        ) : address.detailedData.buildingType === 'Apartment building' ? (
+                            <Icon icon="office" iconSize={20} className="table-icon" />
+                        ) : (
+                                    <Icon icon="home" iconSize={20} className="table-icon" />
+                                )}
+                    </td>
+                    <td className="text-row">
+                        {address.street} {address.house}{address.apartment ? `-${address.apartment}` : ''}
+                    </td>
+                    <td className="text-row">
+                        {address.house}, {address.street}, {address.county}, {address.country}
+                    </td>
+                    <td>{address.detailedData.landTaxValue.toLocaleString('en-US')}</td>
+                    <td></td>
+                </tr>
+            ))
+            ]
+        );
+
+        return (<tbody key={taxInfo.name + '_tbody'}>{rowArray}</tbody>)
+    }
+
+    handleExpandAddressesTable = (taxInfo, owner) => {
+        var taxInfoIndex = this.state.taxInfoList.indexOf(taxInfo);
+        if (taxInfoIndex !== -1) {
+            var ownerIndex = this.state.taxInfoList[taxInfoIndex].indebtedOwners.indexOf(owner);
+            owner.isAddressesExpanded = !owner.isAddressesExpanded;
+            var taxInfoList = this.state.taxInfoList;
+            taxInfoList[taxInfoIndex].indebtedOwners[ownerIndex] = owner;
+            this.setState({ taxInfoList: taxInfoList });
+        }
+    };
+
+    handleExpandDebtorsTable = taxInfo => {
+        var index = this.state.taxInfoList.indexOf(taxInfo);
+        if (index !== -1) {
+            taxInfo.expanded = !taxInfo.expanded;
+            if (!taxInfo.expanded) {
+                taxInfo.indebtedOwners.forEach(
+                    owner => { owner.isAddressesExpanded = false; }
+                );
+            }
+            var taxInfoList = this.state.taxInfoList;
+            taxInfoList[index] = taxInfo;
+            this.setState({ taxInfoList: taxInfoList });
+        }
+    };
 
     calculateTotal = () => {
         const initialValue = {
@@ -135,11 +289,14 @@ class MonthStats extends Component {
 
         let color = d3.scale.ordinal()
             .domain(["Paid", "Missing"])
-            .range(["#008C8C", "#FF495B"]);
+            .range(["#098BBA", "#FF495B"]);
 
         let tooltip = function (name, previousValue, currentValue) {
             return currentValue;
         };
+
+
+        var barChartWidth = this.state.parentWidth > 1380 ? 700 : this.state.parentWidth;
 
         return (
             <div className="chart">
@@ -147,17 +304,19 @@ class MonthStats extends Component {
                 <BarChart
                     data={data}
                     colorScale={color}
-                    width={700}
+                    width={barChartWidth}
                     height={400}
                     tooltipHtml={tooltip}
-                    margin={{ top: 10, bottom: 75, left: 90, right: 10 }}
+                    margin={{ top: 5, bottom: 75, left: 80, right: 10 }}
                     xAxis={{ tickDirection: 'diagonal' }}
                 />
-                <div className="legend">
-                    <p><span className="key-dot paid"></span>Paid</p>
-                </div>
-                <div className="legend">
-                    <p><span className="key-dot unpaid"></span>Unpaid</p>
+                <div className="legends">
+                    <div className="legend">
+                        <p><span className="key-dot paid"></span>Paid</p>
+                    </div>
+                    <div className="legend">
+                        <p><span className="key-dot unpaid"></span>Unpaid</p>
+                    </div>
                 </div>
             </div>
         );
